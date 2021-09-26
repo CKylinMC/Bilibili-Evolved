a422c5a65cbecad09914bc837f4a2a72670dda06
CKylinMC <canszj@yeah.net>
2021-09-26 12:15:10 +0000

    fix: show-part-name

--- registry/lib/components/utils/show-part-name/index.ts
+++ registry/lib/components/utils/show-part-name/index.ts
@@ -3,13 +3,13 @@ import { addStyle, removeStyle } from '@/core/style'
 import { videoUrls } from '@/core/utils/urls'
 import { videoChange } from '@/core/observer'
 import style from './showid.scss'
-import { injectPartNameToPage } from './showid'
+import { registerVideoChangeHandler,unregisterVideoChangeHandler,uninject } from './showid'
 
 const name = 'showPartName'
 const entry = () => {
   addStyle(style, name)
   videoChange(async () => {
-    injectPartNameToPage(unsafeWindow)
+    registerVideoChangeHandler(unsafeWindow)
   })
 }
 export const component: ComponentMetadata = {
@@ -17,7 +17,10 @@ export const component: ComponentMetadata = {
   entry,
   reload: entry,
   unload: () => {
+    unregisterVideoChangeHandler(unsafeWindow)
+    uninject(unsafeWindow)
     removeStyle(name)
+
   },
   displayName: '显示视频分P名',
   tags: [
--- registry/lib/components/utils/show-part-name/showid.ts
+++ registry/lib/components/utils/show-part-name/showid.ts
@@ -3,6 +3,8 @@
 import { select } from '@/core/spin-query'
 import { playerReady } from '@/core/utils'
 
+let ob;
+
 const getAPI = (bvid:string) => fetch(`https://api.bilibili.com/x/web-interface/view?bvid=${bvid}`).then(raw => raw.json())
 const getAidAPI = (aid:string) => fetch(`https://api.bilibili.com/x/web-interface/view?aid=${aid}`).then(raw => raw.json())
 const getUrlParam = (key:string) => (new URL(location.href)).searchParams.get(key)
@@ -26,10 +28,30 @@ const getPageFromCid = (cid, infos) => {
   return 1
 }
 
+export const registerVideoChangeHandler = async (context:Window) => {
+    const video = await select(".bilibili-player-video video");
+    if (!video) return;
+    unregisterVideoChangeHandler(context);
+    const observer = new MutationObserver(async e => {
+        if (e[0].target.src) {
+            injectPartNameToPage(context);
+        }
+    });
+    observer.observe(video, { attributes: true, childList: false });
+    ob = observer;
+}
+
+export const unregisterVideoChangeHandler = async (context:Window) => {
+    ob && ob.disconnect();
+}
+
+export const uninject = (context:Window)=>{
+    select("#bilibiliShowPN-be").then(e=>e.remove())
+}
+
 export const injectPartNameToPage = async (context:Window) => {
   if (document.querySelector('#bilibiliShowInfos')) { return }
   await playerReady()
-  console.log('showid','player ready')
   let infos
   const name = 'bilibiliShowPN-be'
   if (location.pathname.startsWith('/medialist')) {
@@ -44,19 +66,16 @@ export const injectPartNameToPage = async (context:Window) => {
     infos = context.vd || (await getAPI(context.bvid)).data
   }
   infos.p = getUrlParam('p') || getPageFromCid(context.cid, infos)
-  console.log('showid','infos ready',infos)
 
   const av_infobar = await select('.video-data')
   if (!av_infobar) { return }
   const av_root = getOrNew(name, av_infobar as HTMLElement) as HTMLElement;
-  console.log('showid','dom ready',av_root)
   let part: { part: string | any[] }
   try {
     part = infos.pages[infos.p - 1]
   } catch (e) {
     part = infos.pages[0]
   }
-  console.log('showid','partinfo ready',part)
   const currentPageName = part.part.length ? part.part : ''
   let currentPageNum, delimiters
   if (infos.videos !== 1) {
@@ -68,5 +87,4 @@ export const injectPartNameToPage = async (context:Window) => {
   }
   av_root.title = currentPageNum + delimiters[0] + currentPageName
   av_root.innerText = currentPageNum + delimiters[1] + currentPageName
-  console.log('showid','content ready',av_root.innerText)
 }
