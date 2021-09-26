967d9478697cd42586a369630c2c23d23f4037af
CKylinMC <canszj@yeah.net>
2021-09-26 09:52:10 +0000

    feat: show-part-name component

---
+++ registry/lib/components/utils/show-part-name/index.ts
@@ -0,0 +1,31 @@
+import { ComponentMetadata } from '@/components/types'
+import { addStyle,removeStyle } from '@/core/style'
+import { videoUrls } from '@/core/utils/urls'
+import { videoChange } from '@/core/observer'
+import style from './showid.scss'
+import { injectPartNameToPage } from './showid'
+
+const name = 'showPartName'
+const entry = () => {
+  addStyle(style, name);
+  videoChange(async () => {
+    injectPartNameToPage();
+  })
+}
+export const component: ComponentMetadata = {
+  name,
+  entry,
+  reload: entry,
+  unload: () => {
+    removeStyle(name)
+  },
+  displayName: '显示分P名',
+  tags: [
+    componentsTags.video,
+    componentsTags.utils,
+  ],
+  description: {
+    'zh-CN': '在视频信息栏显示分P名',
+  },
+  urlInclude: videoUrls,
+}
