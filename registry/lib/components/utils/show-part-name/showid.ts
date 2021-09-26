42bf99a6a6d3736cc324d47df0431c84c2e01b0c
CKylinMC <canszj@yeah.net>
2021-09-26 11:15:49 +0000

    CI build

--- registry/lib/components/utils/show-part-name/showid.ts
+++ registry/lib/components/utils/show-part-name/showid.ts
@@ -46,7 +46,7 @@ export const injectPartNameToPage = async (context:Window) => {
     const apidata = await getAidAPI(aid)
     infos = apidata.data
   } else {
-    infos = context.vd || (await getAPI(context.bvid)).data;
+    infos = context.vd || (await getAPI(context.bvid)).data
   }
   infos.p = getUrlParam('p') || getPageFromCid(context.cid, infos)
 
